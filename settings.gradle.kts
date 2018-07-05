09f666dacaf0a1b49bb1eb3338792b81c2a2383d
Adler.Fleurant <Adler.Fleurant@picochange.com>
2018-07-05 17:21:05 -0400

    intellij.platform.resources.en has been added as a gradle module. Fix #5
    changed the root gradle build file so that it doesn't include dependencies and doesn't configure subprojects. Each subprojects has to configure themselves
    
    Fixed #3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,9 @@ rootProject.name = "intellij.idea.community.main"
 enableFeaturePreview("IMPROVED_POM_SUPPORT")
 
 include(
-  "intellij.platform.util.rt"
+  "intellij.platform.util.rt",
+  "intellij.platform.resources.en"
 )
 
 project(":intellij.platform.util.rt").projectDir = file("platform/util-rt")
+project(":intellij.platform.resources.en").projectDir = file("platform/platform-resources-en")
