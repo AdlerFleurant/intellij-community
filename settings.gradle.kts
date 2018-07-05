21f6d9cf2ebca2e6c6c68b420f13461fdbe7ff6c
Adler.Fleurant <Adler.Fleurant@picochange.com>
2018-07-05 16:28:44 -0400

    intellij.platform.util.rt has been added as a gradle module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
 rootProject.name = "intellij.idea.community.main"
 
 enableFeaturePreview("IMPROVED_POM_SUPPORT")
+
+include(
+  "intellij.platform.util.rt"
+)
+
+project(":intellij.platform.util.rt").projectDir = file("platform/util-rt")
