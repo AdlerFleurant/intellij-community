15e03cc356dc41e171c0470e87fa62b83000609a
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-07-07 17:52:29 -0400

    intellij.platform.extensions has been added as a gradle module.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,11 @@ enableFeaturePreview("IMPROVED_POM_SUPPORT")
 include(
   "intellij.platform.util.rt",
   "intellij.platform.resources.en",
-  "intellij.platform.util"
+  "intellij.platform.util",
+  "intellij.platform.extensions"
 )
 
 project(":intellij.platform.util.rt").projectDir = file("platform/util-rt")
 project(":intellij.platform.resources.en").projectDir = file("platform/platform-resources-en")
 project(":intellij.platform.util").projectDir = file("platform/util")
+project(":intellij.platform.extensions").projectDir = file("platform/extensions")
