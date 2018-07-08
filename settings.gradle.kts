dee938ffe14d0a4ad2a98340b037881e4994fa8c
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-07-07 21:23:50 -0400

    intellij.platform.core has been added as a gradle module. Fix #10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,12 @@ include(
   "intellij.platform.util.rt",
   "intellij.platform.resources.en",
   "intellij.platform.util",
-  "intellij.platform.extensions"
+  "intellij.platform.extensions",
+  "intellij.platform.core"
 )
 
 project(":intellij.platform.util.rt").projectDir = file("platform/util-rt")
 project(":intellij.platform.resources.en").projectDir = file("platform/platform-resources-en")
 project(":intellij.platform.util").projectDir = file("platform/util")
 project(":intellij.platform.extensions").projectDir = file("platform/extensions")
+project(":intellij.platform.core").projectDir = file("platform/core-api")
