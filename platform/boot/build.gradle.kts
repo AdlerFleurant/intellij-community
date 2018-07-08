21de396e32561b9a7f4f09f66623db81e3ee485a
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-07-08 15:06:48 -0400

    intellij.platform.boot has been added as a gradle module. Fix #21

---
+++ platform/boot/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+  `java-library`
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_1_10
+  targetCompatibility = JavaVersion.VERSION_1_10
+}
+
+repositories {
+  jcenter()
+  maven("http://dl.bintray.com/jetbrains/intellij-third-party-dependencies")
+}
+
+tasks {
+  "compileJava"(JavaCompile::class) {
+    options.encoding = "UTF-8"
+  }
+}
