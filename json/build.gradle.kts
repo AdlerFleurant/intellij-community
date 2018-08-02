afc8941c8cda9ee0df04097ea86902e045b9d7b0
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-08-01 23:42:54 -0400

    intellij.json has been added as a gradle module. Fix #40

--- json/build.gradle.kts
+++ json/build.gradle.kts
@@ -1,3 +1,25 @@
-plugins{
+plugins {
   `java-library`
+  kotlin("jvm") version "1.2.51"
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_1_10
+  targetCompatibility = JavaVersion.VERSION_1_10
+
+  val sourceSet = sourceSets["main"]
+  sourceSet.java.srcDirs.clear()
+  sourceSet.java.srcDir("src")
+  sourceSet.java.srcDir("gen")
+  sourceSet.resources.srcDirs.clear()
+  sourceSet.resources.srcDir("resources")
+}
+
+dependencies {
+  implementation(project(":intellij.spellchecker"))
+  implementation(project(":intellij.platform.core"))
+  implementation(project(":intellij.platform.ide"))
+  implementation(project(":intellij.platform.lang.impl"))
+  implementation("com.google.guava", "guava", "25.1-jre")
+  implementation("com.google.code.gson", "gson", "2.8.5")
 }
