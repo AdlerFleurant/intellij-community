ff8905b349ba088d4506a40da5a997da87bdd1d0
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-08-02 06:28:35 -0400

    intellij.platform.debugger.impl has been added as a gradle module. Fix #53

--- platform/xdebugger-impl/build.gradle.kts
+++ platform/xdebugger-impl/build.gradle.kts
@@ -1,3 +1,27 @@
 plugins {
   `java-library`
+  kotlin("jvm") version "1.2.51"
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_1_10
+  targetCompatibility = JavaVersion.VERSION_1_10
+
+  val sourceSet = sourceSets["main"]
+  sourceSet.java.srcDirs.clear()
+  sourceSet.java.srcDir("src")
+  sourceSet.resources.srcDirs.clear()
+}
+
+tasks {
+  "compileKotlin"(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+}
+
+dependencies {
+  implementation(project(":intellij.platform.debugger"))
+  implementation(project(":intellij.platform.lang.impl"))
+  implementation(project(":intellij.platform.resources"))
+  implementation(project(":intellij.platform.diff"))
 }
