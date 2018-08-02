d918105e83f86e29fb042dc946c246060ed13c77
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-08-02 06:45:45 -0400

    intellij.relaxng has been added as a gradle module. Fix #55

--- xml/relaxng/build.gradle.kts
+++ xml/relaxng/build.gradle.kts
@@ -1,3 +1,33 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 plugins {
   `java-library`
+  kotlin("jvm") version "1.2.51"
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_1_10
+  targetCompatibility = JavaVersion.VERSION_1_10
+
+  val sourceSet = sourceSets["main"]
+  sourceSet.java.srcDirs.clear()
+  sourceSet.java.srcDir("src")
+  sourceSet.resources.srcDirs.clear()
+  sourceSet.resources.srcDir("resources")
+}
+
+tasks {
+  "compileKotlin"(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+}
+
+dependencies {
+  implementation(project(":intellij.xml.impl"))
+  implementation(project(":intellij.platform.lang"))
+  implementation(project(":intellij.platform.lang.impl"))
+  implementation(project(":intellij.xml.dom"))
+  implementation("thaiopensource", "jing", "20030619")
+  implementation("isorelax", "isorelax", "20030108")
+  implementation(files("lib/rngom-20051226-patched.jar"))
+  implementation(files("lib/trang-core.jar"))
 }
