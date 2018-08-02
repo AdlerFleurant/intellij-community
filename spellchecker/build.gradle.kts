6d2375157e6158989e576b5ff92e6b31aff9574b
Adler Fleurant <Adler.Fleurant@picochange.com>
2018-08-01 23:35:30 -0400

    intellij.spellchecker has been added as a gradle module. Fix #52

--- spellchecker/build.gradle.kts
+++ spellchecker/build.gradle.kts
@@ -1,3 +1,27 @@
 plugins {
   `java-library`
+  kotlin("jvm") version "1.2.51"
+}
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_1_10
+  targetCompatibility = JavaVersion.VERSION_1_10
+
+  val sourceSet = sourceSets["main"]
+  sourceSet.java.srcDirs.clear()
+  sourceSet.java.srcDir("src")
+  sourceSet.java.srcDir("gen")
+  sourceSet.resources.srcDirs.clear()
+  sourceSet.resources.srcDir("resources")
+}
+
+dependencies {
+  implementation(project(":intellij.platform.lang"))
+  implementation(project(":intellij.platform.lang.impl"))
+  implementation(project(":intellij.platform.ide"))
+  implementation(project(":intellij.platform.ide.impl"))
+  implementation(project(":intellij.xml"))
+  implementation(project(":intellij.xml.impl"))
+  implementation(project(":intellij.xml.dom"))
+  implementation("com.google.guava", "guava", "25.1-jre")
 }
